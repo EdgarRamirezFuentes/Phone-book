086de8b4d3cb97e77a49530ffdeef0bda4d08ade
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-11-01 21:56:30 -0600

    Integración de las interfaces de los libros.

--- UI/ui.c
+++ UI/ui.c
@@ -7,6 +7,11 @@ GObject *ventanaAgregarUsuario = NULL;
 GObject *ventanaBuscarUsuario = NULL;
 GObject *ventanaEliminarUsuario = NULL;
 GObject *ventanaModificarUsuario = NULL;
+GObject *ventanaAgregarLibro = NULL;
+GObject *ventanaBuscarLibro = NULL;
+GObject *ventanaEliminarLibro = NULL;
+GObject *ventanaModificarLibro = NULL;
+
 
 int iniciarAplicacion(int argc, char *argv[])
 {
@@ -31,7 +36,11 @@ int iniciarAplicacion(int argc, char *argv[])
 	ventanaBuscarUsuario = gtk_builder_get_object (builder, "ventanaBuscarUsuario");
 	ventanaEliminarUsuario = gtk_builder_get_object (builder, "ventanaEliminarUsuario");
 	ventanaModificarUsuario = gtk_builder_get_object(builder, "ventanaModificarUsuario");
-
+	ventanaAgregarLibro = gtk_builder_get_object(builder, "ventanaAgregarLibro");
+	ventanaBuscarLibro = gtk_builder_get_object(builder, "ventanaBuscarLibro");
+	ventanaEliminarLibro = gtk_builder_get_object(builder, "ventanaEliminarLibro");
+	ventanaModificarLibro = gtk_builder_get_object(builder, "ventanaModificarLibro");
+	
 	/* Connect signal handlers to the constructed widgets. */
 
 	button = gtk_builder_get_object (builder, "btn-usuarios");
@@ -79,20 +88,48 @@ int iniciarAplicacion(int argc, char *argv[])
 	button = gtk_builder_get_object (builder, "btn-regresarModificarUsuario");
 	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaUsuariosDesdeModificarUsuario), NULL);
 
+	button = gtk_builder_get_object (builder, "btn-agregarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaAgregarLibro), NULL);
+       		
+	button = gtk_builder_get_object (builder, "btn-regresarAgregarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaLibrosDesdeAgregarLibro), NULL);
+	
+	button = gtk_builder_get_object (builder, "btn-buscarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaBuscarLibro), NULL);
+
+	button = gtk_builder_get_object (builder, "btn-regresarBuscarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaLibrosDesdeBuscarLibro), NULL);
+
+	button = gtk_builder_get_object (builder, "btn-eliminarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaEliminarLibro), NULL);
+
+	button = gtk_builder_get_object (builder, "btn-regresarEliminarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaLibrosDesdeEliminarLibro), NULL);
+
+	button = gtk_builder_get_object (builder, "btn-modificarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaModificarLibro), NULL);
+
+	button = gtk_builder_get_object (builder, "btn-regresarModificarLibro");
+	g_signal_connect (button, "clicked", G_CALLBACK (abrirVentanaLibrosDesdeModificarLibro), NULL);
+
+
 
 	gtk_main();
 
 	return 0;
 }
+/* Ventanas principales */
 
 void abrirVentanaUsuarios(GtkWidget *boton, gpointer data) {
 	gtk_widget_hide(GTK_WIDGET(ventanaPrincipal));
 	gtk_widget_show(GTK_WIDGET(ventanaUsuarios));
 }
+
 void abrirVentanaLibros(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaPrincipal));
 	gtk_widget_show(GTK_WIDGET(ventanaLibros));
 }
+
 void abrirVentanaPrestamos(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaPrincipal));
 	gtk_widget_show(GTK_WIDGET(ventanaPrestamos));
@@ -108,19 +145,24 @@ void abrirVentanaPrincipalDesdeLibros(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaLibros));
 	gtk_widget_show(GTK_WIDGET(ventanaPrincipal));
 }
+
 void abrirVentanaPrincipalDesdePrestamos(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaPrestamos));
 	gtk_widget_show(GTK_WIDGET(ventanaPrincipal));
 
 }
+
+/* Ventanas de usuarios */
 void abrirVentanaAgregarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaUsuarios));
 	gtk_widget_show(GTK_WIDGET(ventanaAgregarUsuario));
 }
+
 void abrirVentanaUsuariosDesdeAgregarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaAgregarUsuario));
 	gtk_widget_show(GTK_WIDGET(ventanaUsuarios));
 }
+
 void abrirVentanaBuscarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaUsuarios));
 	gtk_widget_show(GTK_WIDGET(ventanaBuscarUsuario));
@@ -130,20 +172,65 @@ void abrirVentanaUsuariosDesdeBuscarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaBuscarUsuario));
 	gtk_widget_show(GTK_WIDGET(ventanaUsuarios));
 }
+
 void abrirVentanaEliminarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaUsuarios));
 	gtk_widget_show(GTK_WIDGET(ventanaEliminarUsuario));
 }
+
 void abrirVentanaUsuariosDesdeEliminarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaEliminarUsuario));
 	gtk_widget_show(GTK_WIDGET(ventanaUsuarios));
 }
+
 void abrirVentanaModificarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaUsuarios));
 	gtk_widget_show(GTK_WIDGET(ventanaModificarUsuario));
 }
+
 void abrirVentanaUsuariosDesdeModificarUsuario(GtkWidget *boton, gpointer data){
 	gtk_widget_hide(GTK_WIDGET(ventanaModificarUsuario));
 	gtk_widget_show(GTK_WIDGET(ventanaUsuarios));
 }
 
+/* Ventanas de libros */
+
+void abrirVentanaAgregarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaLibros));
+	gtk_widget_show(GTK_WIDGET(ventanaAgregarLibro));
+}
+
+void abrirVentanaLibrosDesdeAgregarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaAgregarLibro));
+	gtk_widget_show(GTK_WIDGET(ventanaLibros));
+}
+
+void abrirVentanaBuscarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaLibros));
+	gtk_widget_show(GTK_WIDGET(ventanaBuscarLibro));
+}
+
+void abrirVentanaLibrosDesdeBuscarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaBuscarLibro));
+	gtk_widget_show(GTK_WIDGET(ventanaLibros));
+}
+
+void abrirVentanaEliminarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaLibros));
+	gtk_widget_show(GTK_WIDGET(ventanaEliminarLibro));
+}
+
+void abrirVentanaLibrosDesdeEliminarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaEliminarLibro));
+	gtk_widget_show(GTK_WIDGET(ventanaLibros));
+}
+
+void abrirVentanaModificarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaLibros));
+	gtk_widget_show(GTK_WIDGET(ventanaModificarLibro));
+}
+
+void abrirVentanaLibrosDesdeModificarLibro(GtkWidget *boton, gpointer data){
+	gtk_widget_hide(GTK_WIDGET(ventanaModificarLibro));
+	gtk_widget_show(GTK_WIDGET(ventanaLibros));
+}
--- UI/ui.h
+++ UI/ui.h
@@ -11,22 +11,51 @@ extern GObject *ventanaAgregarUsuario;
 extern GObject *ventanaBuscarUsuario;
 extern GObject *ventanaEliminarUsuario;
 extern GObject *ventanaModificarUsuario;
-
+extern GObject *ventanaAgregarLibro;
+extern GObject *ventanaBuscarLibro;
+extern GObject *ventanaEliminarLibro;
+extern GObject *ventanaModificarLibro;
 
 
 int iniciarAplicacion(int argc, char *argv[]);
+
+/* Ventanas Principales */
+
 void abrirVentanaUsuarios(GtkWidget *, gpointer);
-void abrirVentanaLibros(GtkWidget *, gpointer);
-void abrirVentanaPrestamos(GtkWidget *, gpointer);
 void abrirVentanaPrincipalDesdeUsuarios(GtkWidget *, gpointer);
+
+void abrirVentanaLibros(GtkWidget *, gpointer);
 void abrirVentanaPrincipalDesdeLibros(GtkWidget *, gpointer);
+
+void abrirVentanaPrestamos(GtkWidget *, gpointer);
 void abrirVentanaPrincipalDesdePrestamos(GtkWidget *, gpointer);
+
+/* Ventanas de usuarios */
+
 void abrirVentanaAgregarUsuario(GtkWidget *, gpointer);
 void abrirVentanaUsuariosDesdeAgregarUsuario(GtkWidget *, gpointer);
+
 void abrirVentanaBuscarUsuario(GtkWidget *, gpointer);
 void abrirVentanaUsuariosDesdeBuscarUsuario(GtkWidget *, gpointer);
+
 void abrirVentanaEliminarUsuario(GtkWidget *, gpointer);
 void abrirVentanaUsuariosDesdeEliminarUsuario(GtkWidget *, gpointer);
+
 void abrirVentanaModificarUsuario(GtkWidget *, gpointer);
 void abrirVentanaUsuariosDesdeModificarUsuario(GtkWidget *, gpointer);
+
+/* Ventanas de libros */
+
+void abrirVentanaAgregarLibro(GtkWidget *, gpointer);
+void abrirVentanaLibrosDesdeAgregarLibro(GtkWidget *, gpointer);
+
+void abrirVentanaBuscarLibro(GtkWidget *, gpointer);
+void abrirVentanaLibrosDesdeBuscarLibro(GtkWidget *, gpointer);
+
+void abrirVentanaModificarLibro(GtkWidget *, gpointer);
+void abrirVentanaLibrosDesdeModificarLibro(GtkWidget *, gpointer);
+
+void abrirVentanaEliminarLibro(GtkWidget *, gpointer);
+void abrirVentanaLibrosDesdeEliminarLibro(GtkWidget *, gpointer);
+
 #endif
