697dc522bb58e314a8633b94be6b34adf1776328
Edgar Ramirez <edgar-alejandro-fuentes@hotmail.com>
2019-11-19 16:51:20 -0600

    Modificacion de las estructuras de ArbolB y estructura Libro y Prestamo.

--- UI/arbolB.h
+++ UI/arbolB.h
@@ -1,32 +1,56 @@
 #ifndef __ARBOLB_H__
 #define __ARBOLB_H__
+
 struct Usuario
 {
-    int id;
-    char nombre[100];
-    char ap_materno[100];
-    char ap_paterno[100];
-    char curp[18];
-    int prestamos[3];
+	int id;
+	char nombre[100];
+	char ap_materno[100];
+	char ap_paterno[100];
+	char curp[18];
+	int prestamos[3];
 };
 
 struct Libro
 {
-    char titulo[100];
-    char autor[100];
-    char editorial[100];
-    char isbn[25];
-    int existencia;
+	char titulo[100];
+	char autor[100];
+	char editorial[100];
+	int isbn;
+	int existencia;
 };
 
 struct Prestamo
 {
-    int id;
-    int id_usuario;
-    char isbn[25];
-    char fecha_prestamo[10];
-    char fecha_entrega[10];
+	int id;
+	int id_usuario;
+	int isbn;
+	char fecha_prestamo[10];
+	char fecha_entrega[10];
 };
 
+struct ArbolBUsuario
+{
+	int llaves[1000];
+	unsigned int hijos [1001];
+	int esHoja;
+	int tamanio;
+};
+
+struct ArbolBLibro
+{
+	int llaves[1000];
+	unsigned int hijos [1001];
+	int esHoja;
+	int tamanio;
+};
+
+struct ArbolBPrestamo
+{
+	int llaves[1000];
+	unsigned int hijos [1001];
+	int esHoja;
+	int tamanio;
+};
 
 #endif
--- UI/main.c
+++ UI/main.c
@@ -5,5 +5,8 @@ int main(int argc, char *argv[]){
 	printf("Tamaño del struct Usuario: %d \n", sizeof(struct Usuario));	
 	printf("Tamaño del struct Libro: %d \n", sizeof(struct Libro));	
 	printf("Tamaño del struct Prestamo: %d \n", sizeof(struct Prestamo));	
+	printf("Tamaño del struct ArbolBUsuario %d \n", sizeof(struct ArbolBUsuario));
+	printf("Tamaño del struct ArbolBLibro %d \n", sizeof(struct ArbolBLibro));
+	printf("Tamaño del struct ArbolBPrestamo %d \n", sizeof(struct ArbolBPrestamo));
 	return 0;
 }
